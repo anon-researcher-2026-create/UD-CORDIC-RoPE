f0b3d979b83b4cc3770c89256e9814a24f257037
anon-researcher-2026-create <ichinosechizuru455@gmail.com>
2026-04-29 19:50:49 +0530

    Moving folder to RUN

--- STANDARD 10 Stages_Multiple Lib_Fast Cells/10/genus.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.10-p002_1, built Aug 20 2021 10:13:13
-
-# Date: Wed Apr 22 19:12:32 2026
-# Host: redhatacademy19 (x86_64 w/Linux 4.18.0-553.107.1.el8_10.x86_64) (16cores*24cpus*1physical cpu*12th Gen Intel(R) Core(TM) i9-12900K 30720KB)
-# OS:   Red Hat Enterprise Linux release 8.10 (Ootpa)
-
-source /home/redhatacademy19/Documents/Siddhant/SCRIPTS/standard.tcl
--- STANDARD 10 Stages_Multiple Lib_Fast Cells/8/OUTPUT/outputs_21.10-p002_1/rope_cordic_pair/opt/opt_rope_cordic_pair.preserve.tcl
+++
@@ -1,6 +0,0 @@
-################################################################################
-#       Setting dont_touch and dont_use preserve setting
-################################################################################
-set_db base_cell:HOLDX1 .dont_use true
-set_db base_cell:HOLDX1HVT .dont_use true
-set_db base_cell:HOLDX1LVT .dont_use true
