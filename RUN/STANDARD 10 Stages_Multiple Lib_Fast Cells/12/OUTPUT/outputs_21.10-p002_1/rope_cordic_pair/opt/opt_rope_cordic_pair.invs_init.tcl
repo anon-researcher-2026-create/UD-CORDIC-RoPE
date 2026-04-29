357f996a6c284851bc3b295062de25420a8fde9d
anon-researcher-2026-create <ichinosechizuru455@gmail.com>
2026-04-29 19:54:12 +0530

    Add files via upload

---
+++ RUN/STANDARD 10 Stages_Multiple Lib_Fast Cells/12/OUTPUT/outputs_21.10-p002_1/rope_cordic_pair/opt/opt_rope_cordic_pair.invs_init.tcl
@@ -0,0 +1,15 @@
+################################################################################
+#
+# Init setup file
+# Created by Genus(TM) Synthesis Solution on 04/22/2026 19:12:02
+#
+################################################################################
+if { ![is_common_ui_mode] } { error "ERROR: This script requires common_ui to be active."}
+
+read_mmmc OUTPUT/outputs_21.10-p002_1/rope_cordic_pair/opt/opt_rope_cordic_pair.mmmc.tcl
+
+read_physical -lef {/home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_tech.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_macro.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_multibitsDFF.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_hvt/lef/gsclib045_hvt_macro.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_lvt/lef/gsclib045_lvt_macro.lef}
+
+read_netlist OUTPUT/outputs_21.10-p002_1/rope_cordic_pair/opt/opt_rope_cordic_pair.v.gz
+
+init_design
