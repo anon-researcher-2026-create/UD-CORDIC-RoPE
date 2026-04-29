827aefdf37c7b586734130cc48ca0c56b82a0a6b
anon-researcher-2026-create <ichinosechizuru455@gmail.com>
2026-04-29 19:30:39 +0530

    Add files via upload

---
+++ RUN/CSD UD 5 Stages_Multiple Lib_Fast Cells/9/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 21.10-p002_1, built Aug 20 2021 10:13:13
+
+# Date: Wed Apr 22 18:42:36 2026
+# Host: redhatacademy19 (x86_64 w/Linux 4.18.0-553.107.1.el8_10.x86_64) (16cores*24cpus*1physical cpu*12th Gen Intel(R) Core(TM) i9-12900K 30720KB)
+# OS:   Red Hat Enterprise Linux release 8.10 (Ootpa)
+
+source /home/redhatacademy19/Documents/Siddhant/SCRIPTS/csd_ud.tcl
