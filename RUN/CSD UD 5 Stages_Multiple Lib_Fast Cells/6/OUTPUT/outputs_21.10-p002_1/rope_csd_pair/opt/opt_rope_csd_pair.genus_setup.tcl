6f8e7f0da3e9d97b20b47331fcdd60fe410bd15f
anon-researcher-2026-create <ichinosechizuru455@gmail.com>
2026-04-29 19:27:04 +0530

    Add files via upload

---
+++ RUN/CSD UD 5 Stages_Multiple Lib_Fast Cells/6/OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.genus_setup.tcl
@@ -0,0 +1,91 @@
+################################################################################
+#
+# Genus(TM) Synthesis Solution setup file
+# Created by Genus(TM) Synthesis Solution 21.10-p002_1
+#   on 04/22/2026 18:52:48
+#
+# This file can only be run in Genus Common UI mode.
+#
+################################################################################
+
+
+# This script is intended for use with Genus(TM) Synthesis Solution version 21.10-p002_1
+
+
+# Remove Existing Design
+################################################################################
+if {[::legacy::find -design design:rope_csd_pair] ne ""} {
+  puts "** A design with the same name is already loaded. It will be removed. **"
+  delete_obj design:rope_csd_pair
+}
+
+
+# To allow user-readonly attributes
+################################################################################
+::legacy::set_attribute -quiet force_tui_is_remote 1 /
+
+
+# Libraries
+################################################################################
+::legacy::set_attribute library {/home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/timing/fast_vdd1v2_basicCells.lib /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/timing/fast_vdd1v2_multibitsDFF.lib /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_hvt/timing/fast_vdd1v2_basicCells_hvt.lib /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_lvt/timing/fast_vdd1v2_basicCells_lvt.lib} /
+
+::legacy::set_attribute lef_library {/home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_tech.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_macro.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045/lef/gsclib045_multibitsDFF.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_hvt/lef/gsclib045_hvt_macro.lef /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_lvt/lef/gsclib045_lvt_macro.lef} /
+::legacy::set_attribute qrc_tech_file /home/redhatacademy19/Documents/Siddhant/gsclib045_all_v4.8/gsclib045_tech/qrc/qx/gpdk045.tch /
+
+
+# Design
+################################################################################
+read_netlist -top rope_csd_pair OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.v.gz
+read_metric -id current OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.metrics.json
+
+phys::read_script OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.g.gz
+puts "\n** Restoration Completed **\n"
+
+
+# Data Integrity Check
+################################################################################
+# program version
+if {"[string_representation [::legacy::get_attribute program_version /]]" != "21.10-p002_1"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-91] "golden program_version: 21.10-p002_1  current program_version: [string_representation [::legacy::get_attribute program_version /]]"
+}
+# license
+if {"[string_representation [::legacy::get_attribute startup_license /]]" != "Genus_Synthesis"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-91] "golden license: Genus_Synthesis  current license: [string_representation [::legacy::get_attribute startup_license /]]"
+}
+# slack
+set _slk_ [::legacy::get_attribute slack design:rope_csd_pair]
+if {[regexp {^-?[0-9.]+$} $_slk_]} {
+  set _slk_ [format %.1f $_slk_]
+}
+if {$_slk_ != "1364.7"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-92] "golden slack: 1364.7,  current slack: $_slk_"
+}
+unset _slk_
+# multi-mode slack
+# tns
+set _tns_ [::legacy::get_attribute tns design:rope_csd_pair]
+if {[regexp {^-?[0-9.]+$} $_tns_]} {
+  set _tns_ [format %.0f $_tns_]
+}
+if {$_tns_ != "0"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-92] "golden tns: 0,  current tns: $_tns_"
+}
+unset _tns_
+# cell area
+set _cell_area_ [::legacy::get_attribute cell_area design:rope_csd_pair]
+if {[regexp {^-?[0-9.]+$} $_cell_area_]} {
+  set _cell_area_ [format %.0f $_cell_area_]
+}
+if {$_cell_area_ != "2051"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-92] "golden cell area: 2051,  current cell area: $_cell_area_"
+}
+unset _cell_area_
+# net area
+set _net_area_ [::legacy::get_attribute net_area design:rope_csd_pair]
+if {[regexp {^-?[0-9.]+$} $_net_area_]} {
+  set _net_area_ [format %.0f $_net_area_]
+}
+if {$_net_area_ != "989"} {
+   mesg_send [::legacy::find -message /messages/PHYS/PHYS-92] "golden net area: 989,  current net area: $_net_area_"
+}
+unset _net_area_
---
+++ RUN/CSD UD 5 Stages_Multiple Lib_Fast Cells/6/OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.invs_setup.tcl
@@ -0,0 +1,90 @@
+################################################################################
+#
+# Innovus setup file
+# Created by Genus(TM) Synthesis Solution 21.10-p002_1
+#   on 04/22/2026 18:52:48
+#
+################################################################################
+#
+# Genus(TM) Synthesis Solution setup file
+# This file can only be run in Innovus Common UI mode.
+#
+################################################################################
+
+
+# Version Check
+###########################################################
+
+      namespace eval ::genus_innovus_version_check { 
+        set minimum_version 21
+        set maximum_version 22
+        regexp {\d\d} [get_db program_version] this_version
+        puts "Checking Innovus major version against Genus expectations ..."
+        if { $this_version < $minimum_version || $this_version > $maximum_version } {
+          error "**ERROR: this operation requires Innovus major version to be between '$minimum_version' and '$maximum_version'."
+        }
+      }
+    
+set _t0 [clock seconds]
+puts [format  {%%%s Begin Genus to Innovus Setup (%s)} \# [clock format $_t0 -format {%m/%d %H:%M:%S}]]
+set_db read_physical_allow_multiple_port_pin_without_must_join true
+set_db must_join_all_ports true
+set_db timing_cap_unit 1pf
+set_db timing_time_unit 1ns
+
+
+# Design Import
+################################################################################
+source -quiet /mnt/cadence_tools/GENUS211/tools.lnx86/lib/cdn/rc/edi/innovus_procs_common_ui.tcl
+## Reading FlowKit settings file
+source OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.flowkit_settings.tcl
+
+source OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.invs_init.tcl
+
+# Reading metrics file
+################################################################################
+read_metric -id current OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.metrics.json
+## Reading Innovus Mode attributes file
+pqos_eval {rcp::read_taf OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.mode_attributes.taf.gz}
+
+## Reading common preserve file for dont_touch and dont_use preserve settings
+source -quiet OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.preserve.tcl
+
+
+
+# Mode Setup
+################################################################################
+source OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.mode
+
+
+# MSV Setup
+################################################################################
+
+# Source cell padding from Genus
+################################################################################
+source -quiet OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.cell_pad.tcl 
+
+
+# Reading write_name_mapping file
+################################################################################
+
+      if { [is_attribute -obj_type port original_name] &&
+           [is_attribute -obj_type pin original_name] &&
+           [is_attribute -obj_type pin is_phase_inverted]} {
+        source OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.wnm_attrs.tcl
+      }
+    
+
+# Reading NDR file
+source OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.ndr.tcl
+
+# Reading minimum routing layer data file
+################################################################################
+eval_legacy {gpsPrivate::readMinLayerCstr -file OUTPUT/outputs_21.10-p002_1/rope_csd_pair/opt/opt_rope_csd_pair.min_layer} 
+
+eval_legacy {set edi_pe::pegConsiderMacroLayersUnblocked 1}
+eval_legacy {set edi_pe::pegPreRouteWireWidthBasedDensityCalModel 1}
+
+      set _t1 [clock seconds]
+      puts [format  {%%%s End Genus to Innovus Setup (%s, real=%s)} \# [clock format $_t1 -format {%m/%d %H:%M:%S}] [clock format [expr {28800 + $_t1 - $_t0}] -format {%H:%M:%S}]]
+    
